1e6bb7e82cf437de6bf09445d5ca0ba280922249
KwintennIsMe <122505466+KwintennIsMe@users.noreply.github.com>
2023-02-14 11:35:17 -0800

    Complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(wordToRepeat, numberOfTimes) {
+    let newWord = "";
+    if (numberOfTimes > 0) {
+        for (let i = 0; i < numberOfTimes; i++) {
+            newWord += wordToRepeat;
+        }
+    } else if (numberOfTimes < 0) {
+        newWord = "ERROR";
+    }
+    return newWord;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
