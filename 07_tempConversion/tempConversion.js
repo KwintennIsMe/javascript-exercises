bb2b550d542d3edde7c9d2ee0078b4c3207ed411
KwintennIsMe <122505466+KwintennIsMe@users.noreply.github.com>
2023-02-14 17:44:29 -0800

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let newCels = 0;
+  newCels = (fahrenheit - 32) * 5 / 9;
+  newCels = Math.round(newCels * 10) / 10;
+  return newCels;
+  //declare variable newCels
+  //newCels = (fahrenheit - 32) * 5 / 9
+  //round newCels to one decimal point
+  //return newCels
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let newFah = 0;
+  newFah = (celsius * 9 / 5) + 32;
+  newFah = Math.round(newFah * 10) / 10;
+  return newFah;
 };
 
 // Do not edit below this line
