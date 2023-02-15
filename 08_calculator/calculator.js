b627fad30b77aec9d52db9744769b61b0a7daece
KwintennIsMe <122505466+KwintennIsMe@users.noreply.github.com>
2023-02-14 18:07:57 -0800

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  let result = firstNum + secondNum;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+  let result = firstNum - secondNum;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arrayForSum) {
+  let sum = 0;
+  for (let i = 0; i < arrayForSum.length; i++) {
+    sum += arrayForSum[i];
+  }
+  return sum;
+  //declare integer sum
+  //for loop with i = 0, have i < arrayForSum length, have i increment one
+    //have sum = sum + arrayForSum[i]
+  //return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arrayForResult) {
+  let result = arrayForResult[0];
+  for (let i = 1; i < arrayForResult.length; i++) {
+    result *= arrayForResult[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(baseNum, power) {
+  let result = baseNum ** power;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+      console.log(result);
+    }
+  return result;
 };
 
 // Do not edit below this line
