5905977e2e7f6070a9d1df1e1b5e64abfe8db5d8
KwintennIsMe <122505466+KwintennIsMe@users.noreply.github.com>
2023-02-16 20:45:28 -0800

    Finish exercise 10 and work on exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    sentence = sentence.toUpperCase();
+    const sentenceArray = sentence.split('');
+    console.log(sentenceArray);
+    const reverseSentence = sentenceArray.slice();
+    reverseSentence.reverse();
+    console.log(reverseSentence, sentenceArray);
+    if (sentenceArray.toString() === reverseSentence.toString()) {
+        return true;
+    } else {
+        return false;
+    }
+    //convert each letter of string to array
+    //declare variable to hold reversed array
+    //if array = reversed array
+        //return true
+    //else
+        //false
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(fiboNumber) {
+    let fibo = [1, 1];
+    if (fiboNumber >= 0) {
+        for (let i = 2; i <= fiboNumber; i++) {
+            fibo[i] = fibo[i - 2] + fibo[i - 1];
+        }
+        return fibo[fiboNumber - 1];
+    } else {
+        let errorMessage = "OOPS";
+        return errorMessage;
+    }
 };
 
 // Do not edit below this line
